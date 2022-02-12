a86117fbe183e67f30372ff0c02f337bc17ca067
jifishma <44071786+jifishma@users.noreply.github.com>
2022-02-12 01:48:31 -0500

    * More Client presentation cleanup * Include Double defimal format to 2 places * Handle SocketException for random client disconnects * Break out some functionality into separate methods to keep code a little cleaner * Handle FileNotFound exceptions for reading solutions file * Implement writing to solution files * Simplify solve parameter handling * Replace "Placeholder" responses with actual results

--- src/Client/Client.java
+++ src/Client/Client.java
@@ -7,23 +7,26 @@ import java.net.Socket;
 import java.util.Scanner;
 
 public class Client {
-    private static final String SERVER_HOST = "localhost";
-    private static final int SERVER_PORT = 3000;
+    private static final String serverHost = "localhost";
+    private static final int serverPort = 3000;
+
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        try (Socket socket = new Socket(SERVER_HOST, SERVER_PORT)) {
+        try (Socket socket = new Socket(serverHost, serverPort)) {
             DataOutputStream request = new DataOutputStream(socket.getOutputStream());
             DataInputStream response = new DataInputStream(socket.getInputStream());
             String message;
 
+            System.out.println("Connection established with server.");
+
             while (true) {
-                System.out.print("Send command to server:\t");
+                System.out.print("C:\t");
                 message = input.nextLine();
                 request.writeUTF(message);
 
                 String reply = response.readUTF();
-                System.out.println("Server says: " + reply);
+                System.out.println("S:\t" + reply + "\n");
 
                 if ((message.equalsIgnoreCase("logout") || message.equalsIgnoreCase("shutdown"))
                         && reply.equalsIgnoreCase("200 OK")) {
--- src/Server/Server.java
+++ src/Server/Server.java
@@ -4,11 +4,16 @@ import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.EOFException;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.Writer;
 import java.lang.System.Logger;
 import java.lang.System.Logger.Level;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.net.SocketException;
+import java.text.DecimalFormat;
 import java.text.MessageFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -16,13 +21,14 @@ import java.util.List;
 import java.util.Scanner;
 
 public class Server {
-    private static final Logger LOGGER = System.getLogger(Server.class.getName());
-    private static final CredsManager CREDS_MANAGER = CredsManager.getInstance();
-    private static final int SERVER_PORT = 3000;
+    private static final Logger logger = System.getLogger(Server.class.getName());
+    private static final CredsManager credManager = CredsManager.getInstance();
+    private static final DecimalFormat df = new DecimalFormat("0.00");
+    private static final int serverPort = 3000;
 
-    private static void initializeSockets() {
-        try (ServerSocket serverSocket = new ServerSocket(SERVER_PORT)) {
-            LOGGER.log(Level.INFO, "Server started.\n");
+    public static void main(String[] args) {
+        try (ServerSocket serverSocket = new ServerSocket(serverPort)) {
+            logger.log(Level.INFO, "Server started.\n");
 
             boolean shutdown = false;
             do {
@@ -30,9 +36,9 @@ public class Server {
                 shutdown = runServer(clientConn);
             } while (!shutdown);
 
-            LOGGER.log(Level.INFO, "Server shut down.");
+            logger.log(Level.INFO, "Server shut down.");
         } catch (Exception e) {
-            LOGGER.log(Level.ERROR, "Something really bad happened and the Server unexpectedly stopped.");
+            logger.log(Level.ERROR, "Something really bad happened and the Server unexpectedly stopped.");
             e.printStackTrace();
         }
     }
@@ -42,12 +48,13 @@ public class Server {
         DataOutputStream response = new DataOutputStream(clientConn.getOutputStream());
 
         File solutionsFile = null;
+        Writer solutionsFileWriter = null;
 
         try {
             while (true) {
                 response.flush();
                 String message = request.readUTF();
-                LOGGER.log(Level.TRACE, "[{0}]: {1}", CREDS_MANAGER.getAuthenticatedUser(), message);
+                logger.log(Level.INFO, "[{0}]: {1}", credManager.getAuthenticatedUser(), message);
 
                 List<String> args = new ArrayList<>();
                 args.addAll(Arrays.asList(message.split(" ")));
@@ -56,7 +63,7 @@ public class Server {
 
                 // Ensure the user is logged in before executing on a command.
                 if (!command.isBlank() && !command.equals("login") && !command.equals("logout")
-                        && CREDS_MANAGER.getAuthenticatedUser().equals("")) {
+                        && credManager.getAuthenticatedUser().equals("")) {
                     response.writeUTF("ERROR: You must be logged in to execute this command.");
                     continue;
                 }
@@ -67,10 +74,14 @@ public class Server {
                             String username = args.get(0);
                             String password = args.get(1);
 
-                            if (CREDS_MANAGER.authenticateUser(username, password)) {
-                                solutionsFile = new File("solutions/" + username + "_solutions.txt");
+                            if (credManager.authenticateUser(username, password)) {
+                                solutionsFile = new File("solutions\\" + username + "_solutions.txt");
                                 solutionsFile.getParentFile().mkdir();
                                 solutionsFile.createNewFile();
+
+                                if (solutionsFile.exists() && solutionsFile.canWrite()) {
+                                    solutionsFileWriter = new FileWriter(solutionsFile, true);
+                                }
 
                                 response.writeUTF("SUCCESS");
                             } else {
@@ -81,105 +92,17 @@ public class Server {
                         }
                         break;
                     case "solve":
-                        boolean isRectangle = args.contains("-r");
-                        boolean isCircle = args.contains("-c");
-
-                        if ((!isRectangle && !isCircle) || (isRectangle && isCircle)) {
-                            response.writeUTF("ERROR: Invalid operation(s) specified");
-                            break;
-                        } else {
-                            args.remove(0);
-                        }
-
-                        if (args.isEmpty()) {
-                            if (isRectangle) {
-                                response.writeUTF("ERROR: No sides found");
-                            } else if (isCircle) {
-                                response.writeUTF("ERROR: No radius found");
-                            }
-                            break;
-                        }
-
-                        Double a = null;
-                        Double b = null;
-                        try {
-                            a = Double.valueOf(args.get(0));
-
-                            if (args.size() >= 2) {
-                                b = Double.valueOf(args.get(1));
-                            }
-                        } catch (NumberFormatException e) {
-                            response.writeUTF("301 message format error");
-                            break;
-                        }
-
-                        // solve.
-                        Double area = null;
-                        Double perimeter = null;
-                        if (isRectangle) {
-                            if (b != null) {
-                                area = a * b;
-                                perimeter = 2 * (a + b);
-                            }
-
-                            String solution = MessageFormat.format("{0, number, #.##} {1, number, #.##}", area,
-                                    perimeter);
-                            response.writeUTF(solution);
-                            break;
-                        } else if (isCircle) {
-                            area = Math.PI * Math.pow(a, 2);
-                            perimeter = 2 * Math.PI * a;
-
-                            String solution = MessageFormat.format("{0, number, #.##} {1, number, #.##}", area,
-                                    perimeter);
-                            response.writeUTF(solution);
-                            break;
-                        }
-
-                        response.writeUTF("ERROR");
+                        solve(args, response, solutionsFileWriter);
                         break;
                     case "list":
-                        String authedUser = CREDS_MANAGER.getAuthenticatedUser();
-
-                        if (args.contains("-all")) {
-                            if (!authedUser.equalsIgnoreCase("root")) {
-                                response.writeUTF("FAILURE: This method is only accessible to the root user");
-                                break;
-                            }
-
-                            StringBuilder result = new StringBuilder();
-
-                            for (String username : CREDS_MANAGER.getAllUsernames()) {
-                                result.append(MessageFormat.format("{0}\n", username));
-
-                                File userSolutionsFile = new File("solutions/" + username + ".txt");
-                                if (userSolutionsFile.exists()) {
-                                    try (Scanner reader = new Scanner(userSolutionsFile)) {
-                                        if (!reader.hasNextLine()) {
-                                            result.append("\tNo interactions yet.\n");
-                                            continue;
-                                        }
-
-                                        while (reader.hasNextLine()) {
-                                            result.append(MessageFormat.format("\t{0}\n", reader.nextLine()));
-                                        }
-                                    }
-                                } else {
-                                    result.append("\tNo interactions yet.\n");
-                                }
-                            }
-
-                            break;
-                        }
-
-                        response.writeUTF("Placeholder.");
+                        list(args, response);
                         break;
                     case "shutdown":
-                        LOGGER.log(Level.INFO, "Server shutting down.");
+                        logger.log(Level.INFO, "Server shutting down.");
                         response.writeUTF("200 OK");
                         return true;
                     case "logout":
-                        CREDS_MANAGER.logoutUser();
+                        credManager.logoutUser();
                         response.writeUTF("200 OK");
                         return false;
                     default:
@@ -187,15 +110,139 @@ public class Server {
                         break;
                 }
             }
-        } catch (EOFException e) {
-            LOGGER.log(Level.WARNING, "The client's connection has dropped.");
+        } catch (EOFException | SocketException e) {
+            logger.log(Level.WARNING, "The client's connection has dropped.");
             return false;
         } finally {
+            if (solutionsFileWriter != null) {
+                solutionsFileWriter.close();
+            }
+
             clientConn.close();
         }
     }
 
-    public static void main(String[] args) {
-        initializeSockets();
+    private static void solve(List<String> args, DataOutputStream response, Writer writer) throws IOException {
+        boolean isRectangle = args.contains("-r");
+        boolean isCircle = args.contains("-c");
+
+        Double first = null;
+        Double second = null;
+        Double area = null;
+        Double perimeter = null;
+
+        if ((!isRectangle && !isCircle) || (isRectangle && isCircle)) {
+            response.writeUTF("ERROR: Invalid operation(s) specified");
+            return;
+        } else {
+            args.remove(0);
+        }
+
+        if (args.isEmpty()) {
+            String result = "";
+            if (isRectangle) {
+                result = "ERROR: No sides found";
+            } else if (isCircle) {
+                result = "ERROR: No radius found";
+            }
+
+            writer.append(result);
+            writer.append("\n");
+            writer.flush();
+            
+            response.writeUTF(result);
+            return;
+        }
+
+        try {
+            first = Double.valueOf(args.get(0));
+
+            if (args.size() >= 2) {
+                second = Double.valueOf(args.get(1));
+            } else {
+                second = first;
+            }
+        } catch (NumberFormatException e) {
+            response.writeUTF("301 message format error");
+            return;
+        }
+
+        // solve.
+        if (isRectangle) {
+            area = first * second;
+            perimeter = 2 * (first + second);
+
+            String solution = MessageFormat.format(
+                    "Rectangle''s perimeter is {0} and area is {1}",
+                    df.format(perimeter), df.format(area));
+
+            writer.append("sides " + first + " " + second + ":\t" + solution);
+            writer.append("\n");
+            writer.flush();
+
+            response.writeUTF(solution);
+            return;
+        } else if (isCircle) {
+            area = Math.PI * Math.pow(first, 2);
+            perimeter = 2 * Math.PI * first;
+
+            String solution = MessageFormat.format(
+                    "Circle''s circumference is {0} and area is {1}",
+                    df.format(perimeter), df.format(area));
+
+            writer.append("radius " + first + ":\t" + solution);
+            writer.append("\n");
+            writer.flush();
+
+            response.writeUTF(solution);
+            return;
+        }
+
+        response.writeUTF("ERROR: The operation failed spectacularly, and it's unknown why! :(");
+    }
+
+    private static void list(List<String> args, DataOutputStream response) throws IOException {
+        String currentUser = credManager.getAuthenticatedUser();
+        StringBuilder result = new StringBuilder("\n");
+
+        if (args.contains("-all")) {
+            if (!currentUser.equalsIgnoreCase("root")) {
+                response.writeUTF("FAILURE: This method is only accessible to the root user");
+                return;
+            }
+
+            for (String username : credManager.getAllUsernames()) {
+                result.append(MessageFormat.format("\t{0}\n", username));
+                result.append(getInteractions(username));
+            }
+
+            response.writeUTF(result.toString());
+            return;
+        }
+
+        result.append(getInteractions(currentUser));
+        response.writeUTF(result.toString());
+    }
+
+    private static String getInteractions(String username) throws FileNotFoundException {
+        StringBuilder result = new StringBuilder();
+        File userSolutionsFile = new File("solutions\\" + username + "_solutions.txt");
+
+        if (userSolutionsFile.exists()) {
+            try (Scanner reader = new Scanner(userSolutionsFile)) {
+                if (!reader.hasNextLine()) {
+                    result.append("\t\tNo interactions yet\n");
+                    return result.toString();
+                }
+
+                while (reader.hasNextLine()) {
+                    result.append(MessageFormat.format("\t\t{0}\n", reader.nextLine()));
+                }
+            }
+        } else {
+            result.append("\t\tNo interactions yet\n");
+        }
+
+        return result.toString();
     }
 }
